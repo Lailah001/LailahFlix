53153185e1413454309bfbfd1be7665e73d2580b
Lailah <lmweatherspoon0426@gmail.com>
2020-01-27 23:00:37 -0500

    committing changes

---
+++ LailahFlix/MoviesCell.swift
@@ -0,0 +1,15 @@
+//
+//  MoviesCell.swift
+//  LailahFlix
+//
+//  Created by Apayo Apayo on 1/25/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+}
