f8a3136e452248da5c5cd444231feb4345527f50
Lailah <lmweatherspoon0426@gmail.com>
2020-02-09 15:26:27 -0500

    Flix Part 2

---
+++ LailahFlix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  LailahFlix
+//
+//  Created by Apayo Apayo on 2/9/20.
+//  Copyright © 2020 codepath. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
